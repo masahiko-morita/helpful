4fd60f230e33f0ba3bfb4614debc51ef3ea543c7
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-07-04 05:49:56 +0000

    first commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name')->unique();
+            $table->string('password', 60);
+            $table->string('gender');
+            $table->string('content');
             $table->rememberToken();
             $table->timestamps();
         });
     }
+    
 
     /**
      * Reverse the migrations.

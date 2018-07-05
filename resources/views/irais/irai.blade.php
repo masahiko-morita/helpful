d0e836e860451af2343a24f1b2d8f31d3baab0ad
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-05 08:01:14 +0000

    css kari

--- resources/views/irais/irai.blade.php
+++ resources/views/irais/irai.blade.php
@@ -6,9 +6,11 @@
             <div class="panel panel-default-irais">
                 <div class="panel-body-irais">
                     @if ($irai->id)
+                    <div class='title'>
                     <a href="{{ route('irais.show', $irai->id) }}">
                         <p>タイトル:
                             <h4 class="irai-title-irais">{{ $irai->title }}</h4></p></a>
+                    </div>
                     @endif
                     <p>内容:
                     <h4 class="irai-title-irais">{{ $irai->content }}</h4></p>

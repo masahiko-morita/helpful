664dd3a5bb519a4f01e39332bad84637fd2a370d
sakiko.koizumi <sakiko.koizumi@rakuten.com>
2018-07-05 01:22:53 +0000

    aaa

--- resources/views/irais/index.blade.php
+++ resources/views/irais/index.blade.php
@@ -1,11 +1,29 @@
 @extends('layouts.app')
 
+
+
 @section('content')
+
+
+
     <form action="cgi-bin/example.cgi" method="post">
           <p>検索したいキーワードを入力してください。</p>
           <input type="search" name="search" placeholder="キーワードを入力">
           <input type="submit" name="submit" value="検索">
     </form>
     
-   <p>@    include('irais.irais',['irais' => $irais])</p> 
+   <p>@    include('irais.irais',['irais' => $irais])</p>
+   
+   
+   
+   <div>
+  
+        
+        {!! link_to_route('irais.create', '新規メッセージの投稿') !!}
+        
+        
+  </div>
+  
+
+   
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,10 @@
 */
 
 
-<<<<<<< HEAD
-Route::get('/toukou', function() {
-  return view('general.create');
-});
 
 Route::get('/error', 'IraisController@index');
-=======
+
 Route::get('/', 'IraisController@index');
-
->>>>>>> db5766a72de62e0b9676c496f99a6d0b5f0b2014
-
-
            
 
 

85ba107c7c6fd86e363ae26b9ce770159b26a0b8
haruna.nishi <haruna.nishi@rakuten.com>
2018-07-05 03:31:36 +0000

    basic design of irais.show

--- resources/views/irais/show.blade.php
+++ resources/views/irais/show.blade.php
@@ -1,44 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-      
-<div class="container">
-    <div class=text-center>
-        <h1>{{ $irai->title }}</h1>
-    </div>
-    <div class="row text-center">
-        <div class="col-md-4">
-                {{ $irai->timespan }}
-        </div>
-        <div class="col-md-4">
-                {{ $irai->station }}
-        </div>
-        <div class="col-md-4">
-                対価　
-        </div>
-    </div>
-    <div class='text-center'>
-        {{ $irai->content }}
-    </div>
     
-    <div class="row text-center">
-        <div class="col-md-6">
-            {{ $irai->reward }}
-        </div>
-        <div class="col-md-6">
-            <a href="/irai_thankyou">購入する</a>
-        </div>
-    </div>
+<!--<div class="container">-->
+    <!--<div class=text-center>-->
+        <!--<h1>{{ $irai->title }}</h1>-->
+    <!--</div>-->
+    <!--<div class="row text-center">-->
+    <!--    <div class="col-md-4">-->
+    <!--            {{ $irai->timespan }}-->
+    <!--    </div>-->
+    <!--    <div class="col-md-4">-->
+    <!--            {{ $irai->station }}-->
+    <!--    </div>-->
+    <!--    <div class="col-md-4">-->
+    <!--            対価　-->
+    <!--    </div>-->
+    <!--</div>-->
+    <!--<div class='text-center'>-->
+    <!--    {{ $irai->content }}-->
+    <!--</div>-->
+    
+    <!--<div class="row text-center">-->
+    <!--    <div class="col-md-6">-->
+    <!--        {{ $irai->reward }}-->
+    <!--    </div>-->
+        <!--<div class="col-md-6">-->
+        <!--    <a href="/irai_thankyou">購入する</a>-->
+        <!--</div>-->
+    <!--</div>-->
 </div>
+<div class='container text-center'>
 
-<h1>依頼詳細ページ</h1>
+<h1>{{ $irai->title }}</h1>
     <div class="row"> 
+        
         <div class="col-md-6">
             <table class="table table-hover">
-            <tr>
-                <th>タイトル</th>
-                <td>{{ $irai->title }}</td>
-            </tr>
+            
             <tr>
                 <th>説明</th>
                 <td>{{ $irai->content }}</td>
@@ -60,8 +59,19 @@
                 <td>{{ $irai->comment }}</td>
             </tr>
         </table>
+       
         </div> 
-        ppppp
-        
+        <div class="col-md-6">
+             <span><a href="/irai_thankyou">購入する</a></span><br>
+             <span><a href="{{ route('irais.edit', ['id' => $irai->id]) }}">edit</a></span>
+            
+            <span>         
+            {!! Form::open(['route' => ['irais.destroy', $irai->id], 'method' => 'delete']) !!}
+            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+            {!! Form::close() !!}
+            </span>
+        </div>
+       
        </div>
+</div>
 @endsection

3c4a9d67144e28d3227d8b9503fd193fef45c496
masahiko.morita <masahiko.a.morita@rakuten.com>
2018-07-09 05:29:57 +0000

    AAAA

--- resources/views/kus.blade.php
+++ resources/views/kus.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 <div class="text-center">
-  <a href="irais" class="btn btn-default btn-sm" role="button">依頼検索ページに飛ぶ！</a>
+  <a href="irai" class="btn btn-default btn-sm" role="button">依頼検索ページに飛ぶ！</a>
 @endsection

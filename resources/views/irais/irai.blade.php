a612402b512dc6a1f2b2c466b74758cfa5fab363
haruna.nishi <haruna.nishi@rakuten.com>
2018-07-09 05:58:25 +0000

    css

--- resources/views/irais/irai.blade.php
+++ resources/views/irais/irai.blade.php
@@ -1,30 +1,27 @@
-<ul class="media-list">
 @foreach ($irais as $irai)
     <?php $user = $irai->user; ?>
-    <li class="irai">
-        <div class="col-md-4">
-            <div>
-                <a href="{{ route('irais.show', $irai->id) }}">
-                <div class="irai-flower">
-                    <div class='contentbox'>
-                        @if ($irai->id)
-                        <div class='title'>
-                            <p class='widen-height'>
-                                <h3 class="irai-title-irais">{{ $irai->title }}</h3></p>
-                                <h6 class="irai-title-irais">ユーザー名({{ $user->name }})</h6>
-                        </div>
-                        @endif
-                        <div class="irai-contents">
-                            <h5 class="irai-title-irais">お礼➡{{ $irai->reward }}</h5>
-                            <h5 class="irai-title-irais">場所➡{{ $irai->station }}</h5>
-                        </div>
-                    </div>
-                </div>
-                </a>
-            </div>
-        </div>
-    </li>
+<div id="contentContainer" class="trans3d"> 
+    <section id="carouselContainer" class="trans3d">
+        <figure id="item1" class="carouselItem trans3d">
+            <div class="carouselItemInner trans3d">
+                <a href="{{ route('irais.show', $irai->id) }}">    
+             @if ($irai->id)
+       
+               <p>From {{ $user->name }}<br><br>
+               {{ $irai->start }}～{{ $irai->finish }}<br>
+               {{ $irai->title }}<br>
+               
+               {{$irai->station}}周辺<br>
+               お礼: {{ $irai->reward }}<br><br>
+               {{ $irai->comment }}
+               </p>
+               
+               @endif
+            </div></a>
+        </figure>
+     </section>
+</div>
 @endforeach
-</ul>
 
-                    
+
+

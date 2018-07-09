b10b96c0760cbe94c5bbffde3109ba8c2f5d02d7
haruna.nishi <haruna.nishi@rakuten.com>
2018-07-09 07:03:19 +0000

    css

--- resources/views/irais/irai.blade.php
+++ resources/views/irais/irai.blade.php
@@ -1,27 +1,32 @@
-@foreach ($irais as $irai)
-    <?php $user = $irai->user; ?>
-<div id="contentContainer" class="trans3d"> 
-    <section id="carouselContainer" class="trans3d">
-        <figure id="item1" class="carouselItem trans3d">
-            <div class="carouselItemInner trans3d">
-                <a href="{{ route('irais.show', $irai->id) }}">    
-             @if ($irai->id)
-       
-               <p>From {{ $user->name }}<br><br>
-               {{ $irai->start }}～{{ $irai->finish }}<br>
-               {{ $irai->title }}<br>
-               
-               {{$irai->station}}周辺<br>
-               お礼: {{ $irai->reward }}<br><br>
-               {{ $irai->comment }}
-               </p>
-               
-               @endif
-            </div></a>
-        </figure>
-     </section>
-</div>
-@endforeach
+
+
+            @foreach ($irais as $irai)
+                <?php $user = $irai->user; ?>
+                
+                <div class="col-md-3">
+                    <div class='containerirai'>
+                        <p></p>
+                        <div class="maste-box">
+                            <span class='maste-tape'>From {{$user->name}}</span>
+                            <a href="{{ route('irais.show', $irai->id) }}">    
+                         @if ($irai->id)
+                   
+                           <p>
+                           {{ $irai->start }}～{{ $irai->finish }}<br>
+                           {{ $irai->title }}<br>
+                           
+                           {{$irai->station}}周辺<br>
+                           お礼: {{ $irai->reward }}<br><br>
+                           {{ $irai->comment }}
+                           </p>
+                           
+                           @endif
+                        </div></a>
+                    </div>
+                  
+                </div>
+                
+            @endforeach
 
 
 

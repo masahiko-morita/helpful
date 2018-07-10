7f47740d8cbeeda13e32992d7433878e93e797b3
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-07-10 03:34:16 +0000

    comment desighn

--- resources/views/irais/irai.blade.php
+++ resources/views/irais/irai.blade.php
@@ -1,32 +1,30 @@
-
-
-            @foreach ($irais as $irai)
-                <?php $user = $irai->user; ?>
-                
-                <div class="col-md-3">
-                    <div class='containerirai'>
-                        <p></p>
-                        <div class="maste-box">
-                            <span class='maste-tape'>From {{$user->name}}</span>
-                            <a href="{{ route('irais.show', $irai->id) }}">    
-                         @if ($irai->id)
-                   
-                           <p>
-                           {{ $irai->start }}～{{ $irai->finish }}<br>
-                           {{ $irai->title }}<br>
-                           
-                           {{$irai->station}}周辺<br>
-                           お礼: {{ $irai->reward }}<br><br>
-                           {{ $irai->comment }}
-                           </p>
-                           
-                           @endif
-                        </div></a>
-                    </div>
-                  
-                </div>
-                
-            @endforeach
+@foreach ($irais as $irai)
+    <?php $user = $irai->user; ?>
+    
+    <div class="col-md-3">
+        <div class='containerirai'>
+            <p></p>
+            <div class="maste-box">
+                <span class='maste-tape'>From {{$user->name}}</span>
+                <a href="{{ route('irais.show', $irai->id) }}">    
+             @if ($irai->id)
+       
+               <p>
+               {{ $irai->start }}～{{ $irai->finish }}<br>
+               {{ $irai->title }}<br>
+               
+               {{$irai->station}}周辺<br>
+               お礼: {{ $irai->reward }}<br><br>
+               {{ $irai->comment }}
+               </p>
+               
+               @endif
+            </div></a>
+        </div>
+      
+    </div>
+    
+@endforeach
 
 
 

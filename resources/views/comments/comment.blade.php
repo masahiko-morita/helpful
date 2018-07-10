a5b4ae9e2f01282543f6ed210dcd8060609a4876
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-10 05:09:54 +0000

    tukareta

--- resources/views/comments/comment.blade.php
+++ resources/views/comments/comment.blade.php
@@ -1,7 +1,6 @@
 <ul class="media-list">
 @foreach ($comments as $comment)
     <?php $user = $comment->user; ?>
-<<<<<<< HEAD
         <li class="comment">
             <div class="comment-flower">
                 <div class='contentbox'>
@@ -25,39 +24,11 @@
                                 @endif
                             </div>
                         </div>
-                        
-=======
-                <div class="comment-flower">
-                    <div class='contentbox'>
-                        @if ($comment->irai_id == $irai->id)
-                        <div class='comment-title'>
-                            <h3 class="comment-title-comments"><a href="{{ route('users.show', $user->id) }}">{{ $user->name }}</h3></a>
-                                    <div class="popover right show" style="position:relative; max-width:50%;">
-                                        <div class="arrow"></div>
-                                            <div class="popover-content">
-                                                <p>
-                                                   <td><h5 class="comment-title-comments">{{ $comment->content }}</h4></td>
-                                                </p>
-                                            </div>
-                                    </div>
-                        </div>
-                            @if (Auth::user()->id == $comment->user_id)       
-                                {!! Form::open(['route' => ['comments.destroy', $comment->id], 'method' => 'delete']) !!}
-                                {{Form::hidden('irai_id', $irai->id)}}
-                                {!! Form::submit('コメント削除', ['class' => 'btn btn-danger btn-xs']) !!}
-                                {!! Form::close() !!}
-                            @endif 
-                        @endif
-                            
->>>>>>> 7dd9c4f94f354ef2f5c29b72ab4f77347d660c50
                     </div>
                     @endif
                 </div>
-<<<<<<< HEAD
             </div>
         </li>
-=======
->>>>>>> 7dd9c4f94f354ef2f5c29b72ab4f77347d660c50
 @endforeach
 </ul>
 

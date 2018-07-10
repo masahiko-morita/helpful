4c56baa8c0369555393c730c147b6578ba94d742
sakiko.koizumi <sakiko.koizumi@rakuten.com>
2018-07-10 04:39:24 +0000

    ffffd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,18 +5,14 @@
         @include('kus')
     @else
 
-        <div class="content">
+    <div class="content">
         <div class="content-inner">
             <div class="maintitle">
                 <h1>ワンクリックで簡単に投稿</h1>
                 @if (!Auth::check())
-                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">HELPFULを始める</a>
+                    <a href="{{ route('signup.get') }}" class="btn btn-default btn-lg">HELPFULを始める</a>
                 @endif
             </div>
-
-            <div class="content-contents">
-            </div>
-
         </div>
     </div>
     @endif
@@ -25,28 +21,25 @@
         <h2>Helpfulでは、すぐに交換ができて、<br>安心、安全なお取引ができます。</h2>
      </div>
      
-<<<<<<< HEAD
-     
-     
-=======
+
         <div class="steps">
+            <div class="c">
             <h3 class="text-left">
-                              <span class="top-introduction-num">1</span>
+                              <span class="top-introduction-num">1.</span>
                             簡単に投稿できる
             </h3>
-            
-            
-
             
             <img src="/images/toukou.jpg" width="350px" height="300px">
 
             <p class="top-introduction-description">
-              困ったことや最寄りの駅を入力するだけで、<br>簡単に投稿できます。
-            </p>
+              困ったことや最寄りの駅を入力するだけで、<br>簡単に投稿できます。</p>
+            </div>
+            
           
-    
+           <div class="c">
             <h3 class="text-left">
-                              <span class="top-introduction-num">2</span>
+                <div class="c">
+                              <span class="top-introduction-num">2.</span>
                             すぐにリクエストを反映できる
             </h3>
 
@@ -56,8 +49,13 @@
             <p class="top-introduction-description">
               投稿したら即返信できる。
             </p>
+            </div>
+            
+            
+            
+            <div class="c">
             <h3 class="text-left">
-                              <span class="top-introduction-num">3</span>
+                      <span class="top-introduction-num">3.</span>
                             たくさん投稿がのせられる
             </h3>
 
@@ -67,8 +65,9 @@
             <p class="top-introduction-description">
               毎日いくつも依頼が投稿されるので、<br>困ったことがきっと解決します。
             </p>
+            </div>
         </div>
->>>>>>> c09ac124d68f2dc8ceca36d2ed4c98070560e849
+
      <div class=explaination>
           <h2>Helpfulの特徴</h2>
     </div>
@@ -81,7 +80,7 @@
               お手伝いしたいことや、<br>依頼したいことを投稿しよう
             </h3>
             <p class="top-key-features-description text-center">
-              困った時はお手伝いを依頼しよう。暇な時間は人助けしよう。
+              困った時はお手伝いを依頼しよう。暇な時間は人助けしよう
             </p>
           </section>
           
@@ -102,7 +101,7 @@
                                   <center><img src=/images/chat.jpg alt="" width="80px" height="80px"></center>
                               </figure>
                         <h3 class="top-key-features-subtitle text-center">
-              メッセージ機能を活用しよう。
+              メッセージ機能を活用しよう
             </h3>
             <p class="top-key-features-description text-center">
               分からないことは、返信機能でですぐさま解決。
@@ -114,7 +113,7 @@
                                   <center><img src="images/money.png" alt="" width="80px" height="80px"></center>
                               </figure>
                         <h3 class="top-key-features-subtitle text-center">
-              誰でも簡単に無料で楽しめる。
+              誰でも簡単に無料で楽しめる
             </h3>
             <p class="top-key-features-description text-center">
               いつでも誰でも無料で投稿。
@@ -126,12 +125,14 @@
           <!--          <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">HELPFULを始める</a>-->
           <!--      @endif-->
           <!--  </div>-->
-            
-            <div class="animation">
-  <div class="effect"></div>
-  <a href="#"> HELPFULを始める </a>
-</div>
-          
-　　</div>
+    <div class="abc">        
+      <div class="animation">
+        <div class="effect"></div>
+         @if (!Auth::check())
+          <a href="{{ route('signup.get') }}"> HELPFULを始める </a>
+           @endif
+         </div>
+　  　</div>
+　  </div>
 @endsection
 

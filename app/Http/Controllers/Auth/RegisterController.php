be828728939f8b26b3eccdf86974417aa8ed27ee
masahiko.morita <masahiko.a.morita@rakuten.com>
2018-07-11 07:55:46 +0000

    afterlogin

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/irais';
 
     /**
      * Create a new controller instance.

08e7d1706f444a04480a07c2bdba2ed3730656c6
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-11 06:29:52 +0000

    長さ調性しました

--- resources/views/irais/search.blade.php
+++ resources/views/irais/search.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="row">         
-  <div class="content">
-       <span class='form-group'>
+  <div class="search-content">
+       <span class=' form-inline'>
             {!! Form::open(['route' => 'irais.search', 'method' => 'get']) !!}
-            {!! Form::text('q', '', ['class' => 'form-control input-lg', 'placeholder' => 'キーワード(例:タイトル(引っ越し),駅(二子玉),時間(12日の場合12))', 'size' => 40,'style' => 'width:800px']) !!}
-            {!! Form::submit('依頼を検索', ['class' => 'btn btn-success btn-lg']) !!}
+            {!! Form::text('q', '', ['class' => 'form-control input-lg center-block', 'placeholder' => 'キーワード(例:タイトル(引っ越し),駅(二子玉),時間(12日の場合12))', 'size' => 40,'style' => 'width:600px']) !!}
+            {!! Form::submit('依頼を検索', ['class' => 'btn btn-success btn-lg ']) !!}
             {!! Form::close() !!}
         </span>
   </div>

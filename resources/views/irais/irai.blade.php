64c9f5d2765ae96f6c8cd009a30e7a1b51c85db2
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-12 06:53:07 +0000

    sumideru

--- resources/views/irais/irai.blade.php
+++ resources/views/irais/irai.blade.php
@@ -12,28 +12,45 @@
                 <span class='maste-tape'>From {{$user->name}}</span>
                  
              @if ($irai->id)
-       
-               <div class='maste-content'>
-                <br>
-                <h4 class='under'>{{ $irai->title }}</h4>
-
-                <div class='maste-btn'>
-                    @if (Auth::user() == null)
-                
-                    @elseif(Auth::user()->id != $irai->user_id)
-                        {!! Form::open(['route' => ['irais.thankyou', $irai->id], 'method' => 'get']) !!}
-                        {!! Form::submit('手伝う！！！', ['class' => 'btn center-block']) !!}
-                        {!! Form::close() !!}
-                    @endif
-                </div>
-
-                <h4 class="glyphicon glyphicon-time text-center"></h4><br> {{ $irai->start }}~<br>{{ $irai->finish }}<br>
-                <h4 class="glyphicon glyphicon-map-marker"></h4> {{$irai->station}}<br>
-                <h4 class="glyphicon glyphicon-heart"></h4> {{ $irai->reward }}<br>
-               </div>
-
+                    @if (Auth::user()->is_finishing($irai->id))
+                   <div class='maste-content'>
+                            <h4 class='under'>{{ $irai->title }}</h4>
+            
+                            <div class='maste-btn'>
+                                @if (Auth::user() == null)
+                            
+                                @elseif(Auth::user()->id != $irai->user_id)
+                                    <div id="kanryou">
+                                         @include('irai_finish.finish_button', ['user' => $user])
+                                    </div>
+                                @endif
+                            </div>
+            
+                            <h4 class="glyphicon glyphicon-time text-center"></h4><br> {{ $irai->start }}~<br>{{ $irai->finish }}<br>
+                            <h4 class="glyphicon glyphicon-map-marker"></h4> {{$irai->station}}<br>
+                            <h4 class="glyphicon glyphicon-heart"></h4> {{ $irai->reward }}<br>
+                   </div>
+                    @else
+                    <div class='maste-content'>
+                            <br>
+                            <h4 class='under'>{{ $irai->title }}</h4>
+            
+                            <div class='maste-btn'>
+                                @if (Auth::user() == null)
+                            
+                                @elseif(Auth::user()->id != $irai->user_id)
+                                    {!! Form::open(['route' => ['irais.thankyou', $irai->id], 'method' => 'get']) !!}
+                                    {!! Form::submit('手伝う！！！', ['class' => 'btn center-block']) !!}
+                                    {!! Form::close() !!}
+                                @endif
+                            </div>
+            
+                            <h4 class="glyphicon glyphicon-time text-center"></h4><br> {{ $irai->start }}~<br>{{ $irai->finish }}<br>
+                            <h4 class="glyphicon glyphicon-map-marker"></h4> {{$irai->station}}<br>
+                            <h4 class="glyphicon glyphicon-heart"></h4> {{ $irai->reward }}<br>
+                   </div>
+                   @endif
                @endif
-               
             </div>
             </a>
         </div>

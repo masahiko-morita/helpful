42163dabac5badc4989c693be5ac737c52d944d6
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-07-12 00:55:39 +0000

    help button

--- resources/views/irais/irai.blade.php
+++ resources/views/irais/irai.blade.php
@@ -18,16 +18,18 @@
                 <h4 class='under'>{{ $irai->title }}</h4>
 
                 <div class='maste-btn'>
-                    @if (Auth::check())
-                    {!! Form::open(['route' => ['irais.thankyou', $irai->id], 'method' => 'get']) !!}
-                        {!! Form::submit('手伝う！！！', ['class' => 'btn btn-success btn-sm']) !!}
-                    {!! Form::close() !!}
+                    @if (Auth::user() == null)
+                
+                    @elseif(Auth::user()->id != $irai->user_id)
+                        {!! Form::open(['route' => ['irais.thankyou', $irai->id], 'method' => 'get']) !!}
+                            {!! Form::submit('手伝う！！！', ['class' => 'btn btn-success btn-sm']) !!}
+                        {!! Form::close() !!}
                     @endif
                 </div>
 
                 <h4 class="glyphicon glyphicon-time text-center"></h4><br> {{ $irai->start }}~<br>{{ $irai->finish }}<br>
                 <h4 class="glyphicon glyphicon-map-marker"></h4> {{$irai->station}}<br>
-                <h4 class="glyphicon glyphicon-gift"></h4>  {{ $irai->reward }}<br>
+                <h4 class="glyphicon glyphicon-heart"></h4> {{ $irai->reward }}<br>
                </div>
 
                @endif

7c4c9bc89edbf92b060893a195c131796e953f1c
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-12 06:02:18 +0000

    tyousei

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
   
         <div id="sampleCarousel" class="carousel slide" data-ride="carousel">
                  
-         <a href="{{ route('irais.index') }}"">   
+         <a href="{{ route('irais.index') }}">   
         	<div class="carousel-inner" role="listbox">
         		<div class="item active">
         			<img src="../images/feed1.png" alt="First slide">

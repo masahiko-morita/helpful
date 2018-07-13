ea23694ad59ab6db225b9a38258f025eb46c283a
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-13 01:13:47 +0000

    ボタン周り調性

--- resources/views/irais/show.blade.php
+++ resources/views/irais/show.blade.php
@@ -30,13 +30,17 @@
                 </tr>
             </table>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6"> 
             @if (Auth::user()->id != $irai->user_id)
             <div id="tetsudau">
-                 @include('irai_help.help_button', ['user' => $user])
+                @if (Auth::user()->is_finishing($irai->id)) 
+                 @include('irai_finish.finish_button', ['user' => $user])
                 <!--{!! Form::open(['route' => ['irais.thankyou', $irai->id], 'method' => 'get']) !!}-->
                 <!--{!! Form::submit('手伝う！！！', ['class' => 'btn center-block']) !!}-->
                 <!--{!! Form::close() !!}-->
+                @else
+                    @include('irai_help.help_button', ['user' => $user])
+                @endif
             </div>
             @endif
             @if (Auth::user()->id == $irai->user_id)

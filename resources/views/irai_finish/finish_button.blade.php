db0330e1688feb9c6b53f7e549bf759aff96f3aa
momoe.yamane <momoe.yamane@techacademy.jp>
2018-07-13 01:30:14 +0000

    nyoro-kakko

--- resources/views/irai_finish/finish_button.blade.php
+++ resources/views/irai_finish/finish_button.blade.php
@@ -8,7 +8,7 @@
         </div>
     @else
     <div id="kanryou">
-        {{!! Form::open(['route' => ['irai.finish', $irai->id]]) !!}
+        {!! Form::open(['route' => ['irai.finish', $irai->id]]) !!}
             {!! Form::submit('依頼を完了する', ['class' => "btn btn-block center-block"]) !!}
         {!! Form::close() !!}
         </div>

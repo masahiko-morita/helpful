8e535f74242eca72121e565f5e476204e5a01c6f
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-13 00:55:26 +0000

    手伝い中でサンキューページにもどる

--- resources/views/irai_help/help_button.blade.php
+++ resources/views/irai_help/help_button.blade.php
@@ -1,9 +1,8 @@
 @if (Auth::id() != $user->id)
     @if (Auth::user()->is_helping($irai->id))
-
         <div id="kanryou-zumi">
         {!! Form::open(['route' => ['irai.help', $irai->id]]) !!}
-            {!! Form::submit('手伝い中', ['class' => "btn btn-danger btn-block"]) !!}
+        {!! Form::submit('手伝い中', ['class' => "btn btn-danger btn-block"]) !!}
         {!! Form::close() !!}
         </div>
     @else

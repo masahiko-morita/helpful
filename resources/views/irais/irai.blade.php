61f29b27cdc25b2afe1b4bf5089731bcdeca516c
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-13 00:29:45 +0000

    @include

--- resources/views/irais/irai.blade.php
+++ resources/views/irais/irai.blade.php
@@ -30,25 +30,6 @@
                             <h4 class="glyphicon glyphicon-map-marker"></h4> {{$irai->station}}<br>
                             <h4 class="glyphicon glyphicon-heart"></h4> {{ $irai->reward }}<br>
                    </div>
-                    @elseif (Auth::user()->is_helping($irai->id))
-                    <div class='maste-content'>
-                            <br>
-                            <h4 class='under'>{{ $irai->title }}</h4>
-            
-                            <div class='maste-btn'>
-                                @if (Auth::user() == null)
-                            
-                                @elseif(Auth::user()->id != $irai->user_id)
-                                <div id="tetsudai-now">  
-                                    @include('irai_help.help_button', ['user' => $user])
-                                </div>
-                                @endif
-                            </div>
-            
-                            <h4 class="glyphicon glyphicon-time text-center"></h4><br> {{ $irai->start }}~<br>{{ $irai->finish }}<br>
-                            <h4 class="glyphicon glyphicon-map-marker"></h4> {{$irai->station}}<br>
-                            <h4 class="glyphicon glyphicon-heart"></h4> {{ $irai->reward }}<br>
-                   </div> 
                     @else
                     <div class='maste-content'>
                             <br>
@@ -58,9 +39,7 @@
                                 @if (Auth::user() == null)
                             
                                 @elseif(Auth::user()->id != $irai->user_id)
-                                    {!! Form::open(['route' => ['irais.thankyou', $irai->id], 'method' => 'get']) !!}
-                                    {!! Form::submit('手伝う！！！', ['class' => 'btn center-block']) !!}
-                                    {!! Form::close() !!}
+                                    @include('irai_help.help_button', ['user' => $user])
                                 @endif
                             </div>
             

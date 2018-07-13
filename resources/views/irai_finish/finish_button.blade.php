19e9befd9bbaae42672ca178b2c9cd1619c94548
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-07-13 00:28:39 +0000

    daiki

--- resources/views/irai_finish/finish_button.blade.php
+++ resources/views/irai_finish/finish_button.blade.php
@@ -1,9 +1,9 @@
 @if (Auth::id() != $user->id)
     @if (Auth::user()->is_finishing($irai->id))
         <div id="kanryou-zumi">
-        <!--{!! Form::open(['route' => ['irai.unfinish', $irai->id], 'method' => 'delete']) !!}-->
+        {!! Form::open(['route' => ['irai.unfinish', $irai->id], 'method' => 'delete']) !!}
         {!! Form::submit('依頼完了済', ['class' => "btn btn-block center-block"]) !!}
-        <!--{!! Form::close() !!}-->
+        {!! Form::close() !!}
         </div>
     @else
     <div id="kanryou">

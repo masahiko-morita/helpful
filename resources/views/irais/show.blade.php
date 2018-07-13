dfcfb0f51c0a5dae388f277c82260473ae5c8380
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-13 06:48:54 +0000

    ppp

--- resources/views/irais/show.blade.php
+++ resources/views/irais/show.blade.php
@@ -34,11 +34,6 @@
             @if (Auth::user()->id != $irai->user_id)
 
             <div id="tetsudau">
-                 @include('irai_help.help_button', ['user' => $user])
-                <!--{!! Form::open(['route' => ['irais.thankyou', $irai->id], 'method' => 'get']) !!}-->
-                <!--{!! Form::submit('手伝う！！！', ['class' => 'btn center-block']) !!}-->
-                <!--{!! Form::close() !!}-->
-
                 @if (Auth::user()->is_finishing($irai->id)) 
                     <div id ="kanryou-zumi-2">
                     @include('irai_finish.finish_button', ['user' => $user])

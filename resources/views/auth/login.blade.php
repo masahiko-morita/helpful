f3dc2049937f91e951bc14283a6816acecf30f37
sakiko.koizumi <sakiko.koizumi@rakuten.com>
2018-07-18 01:47:51 +0000

    sdfg

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,19 +4,22 @@
     <div class='ribbon2'>
            <h2>ログイン</h2>
     </div>
-
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
 
             {!! Form::open(['route' => 'login.post']) !!}
                 <div class="form-group">
+                    <div class="form-group has-success has-feedback">
                     {!! Form::label('name', 'ニックネーム') !!}
                     {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
+                    <span class="glyphicon glyphicon-ok form-control-feedback"></span>
                 </div>
 
                 <div class="form-group">
+                    <div class="form-group has-error has-feedback">
                     {!! Form::label('password', 'パスワード') !!}
                     {!! Form::password('password', ['class' => 'form-control']) !!}
+                　　 <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                 </div>
 
                 {!! Form::submit('ログイン', ['class' => 'btn btn-info btn-block']) !!}

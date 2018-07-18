845389d53dbf2a46c2aa99329b9ab70f8da9c61c
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-07-18 00:31:14 +0000

    daiki

--- app/User.php
+++ app/User.php
@@ -77,10 +77,6 @@ class User extends Authenticatable
     public function finished()
     {
         
-        // User Irao
-        // irai_finish
-        // user --> finish_id
-        // irai --> user_id
         return $this->belongsToMany(Irai::class, 'irai_finish', 'finish_id', 'user_id')->withTimestamps();
     }
 
--- resources/views/irai_help/help_button.blade.php
+++ resources/views/irai_help/help_button.blade.php
@@ -1,14 +1,14 @@
 @if (Auth::id() != $user->id)
     @if (Auth::user()->is_helping($irai->id))
         <div id="tetsudai-tyu">
-        {!! Form::open(['route' => ['irai.help', $irai->id]]) !!}
-        {!! Form::submit('手伝い中', ['class' => "btn btn-danger btn-block center-block"]) !!}
+        {!! Form::open(['route' => ['irai.finish', $irai->id]]) !!}
+            {!! Form::submit('依頼完了済！', ['class' => "btn btn-block center-block btn-danger"]) !!}
         {!! Form::close() !!}
         </div>
     @else
     <div id="tetsudai">
-        {!! Form::open(['route' => ['irai.help', $irai->id]]) !!}
-            {!! Form::submit('手伝う！', ['class' => "btn center-block"]) !!}
+        {!! Form::open(['route' => ['irai.finish', $irai->id]]) !!}
+            {!! Form::submit('手伝いを完了する！', ['class' => "btn center-block"]) !!}
         {!! Form::close() !!}
         </div>
     @endif

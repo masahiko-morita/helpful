08dc55da4f8eb95f2d70791cd8a3b101070a32d9
sakiko.koizumi <sakiko.koizumi@rakuten.com>
2018-07-20 05:15:29 +0000

    asdfghjk

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,13 +8,8 @@
         <img src="{{ Gravatar::src($user->email, 100) . '&d=mm' }}" alt="" class="img-circle" width='30%'>
         <div id=momo_name>
         <h3>{{ $user->name }}</h3>
-<<<<<<< HEAD
     </div>
     <div class='col-md-5 text-center'>
-=======
-        </div>
-   
->>>>>>> 67943f1dd9b2d0febe8c973b77fd078a6e15f7ca
 		<div class="balloon">
       		{{$user->content}}
 		</div>

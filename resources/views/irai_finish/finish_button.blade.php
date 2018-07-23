5c3f68d76146e6e6be744b0421a5470d4e37c6d6
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-23 06:46:44 +0000

    show 中央寄席しました

--- resources/views/irai_finish/finish_button.blade.php
+++ resources/views/irai_finish/finish_button.blade.php
@@ -1,9 +1,9 @@
     @if ($irai->alive == 0)
     <div id="kaiketsu-zumi">
-        <p class='center-block'>解決済み</p>
+        <p>解決済み</p>
     </div>
     @else
-    <div class='center-block' id="kaiketsu">
+    <div id="kaiketsu">
         {!! Form::open(['route' => ['irai.unfinish', $irai->id], 'method' => 'delete']) !!}
         {{Form::hidden('type', 'message')}}
         {!! Form::submit('解決した', ['class' => 'btn center-block']) !!}
--- resources/views/irais/show.blade.php
+++ resources/views/irais/show.blade.php
@@ -1,26 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class='container'>
     <?php $user = $irai->user; ?>
         <h1 class='text-center'>{{ $irai->title }}</h1>
                 @if (Auth::user()->id == $irai->user_id)
-                    <div id="hensyuu">
-                        {!! Form::open(['route' => ['irais.edit', $irai->id], 'method' => 'get']) !!}
-                            {!! Form::submit('編集', ['class' => 'btn center-block']) !!}
-                        {!! Form::close() !!}
+                    <div class="row">
+                    <div class="col-xs-6">
+                        <div id="hensyuu">
+                            {!! Form::open(['route' => ['irais.edit', $irai->id], 'method' => 'get']) !!}
+                                {!! Form::submit('編集', ['class' => 'btn inline-block']) !!}
+                            {!! Form::close() !!}
+                        </div>
                     </div>
-                    <div id="sakujyo">
-                        {!! Form::open(['route' => ['irais.destroy', $irai->id], 'method' => 'delete']) !!}
-                            {!! Form::submit('削除', ['class' => 'btn center-block']) !!}
-                        {!! Form::close() !!}
+                    <div class="col-xs-6">
+                        <div id="sakujyo">
+                            {!! Form::open(['route' => ['irais.destroy', $irai->id], 'method' => 'delete']) !!}
+                                {!! Form::submit('削除', ['class' => 'btn inline-block']) !!}
+                            {!! Form::close() !!}
+                        </div>
                     </div>
-                    <div>
+                    </div>
+                    <div class="col-6 col-offset-3">
                        @include('irai_finish.finish_button', ['user' => $user])
                     </div>
                 @endif    
     <div class="row">
-        <div class="col-xs-12"> 
+        <div class="col-12 col-offset-1"> 
             <table class="table">
                 <tr>
                     <th><i class="fa fa-user" id='show'> </i></th>
@@ -46,17 +51,15 @@
         </div>
     </div>
         <div class="row row-eq-height">
-            <div class="col-md-9">
                 <div id="toukou">
                     {!! Form::open(['route' => ['comments.store'], 'method' => 'post']) !!}
                         {{Form::hidden('irai_id', $irai->id)}}
                         {{Form::hidden('user_id', $user->id)}}
                         {{Form::hidden('type', 'comment')}}
-                        {!! Form::textarea('content', null, ['class' => 'form-control input-lg', 'rows=2',  'placeholder' => '手伝います！〇月〇日いかがですか？' ]) !!}
-                        {!! Form::submit('メッセージ送信！', ['class' => 'btn btn-success btn-lg']) !!}
+                        {!! Form::textarea('content', null, ['class' => 'form-control input-lg center-block', 'rows=2',  'placeholder' => '手伝います！〇月〇日いかがですか？' ]) !!}
+                        {!! Form::submit('メッセージ送信！', ['class' => 'btn btn-success btn-lg center-blosk']) !!}
                     {!! Form::close() !!}
                 </div>
-            </div>
         </div>    
 @include('comments.comment', ['comments' => $comments])
 

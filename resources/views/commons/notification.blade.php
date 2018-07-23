eb121453fe15be97e616e67dd99f413ba332407a
momoe.yamane <momoe.yamane@techacademy.jp>
2018-07-23 06:55:29 +0000

    toukou

--- resources/views/commons/notification.blade.php
+++ resources/views/commons/notification.blade.php
@@ -73,10 +73,7 @@
     } else {
         $messagesIsShow = true;
     }
-    var_dump($messages_id);
-    var_dump($vvv);
-    var_dump($messagesIsShow);
-    var_dump($_COOKIE);
+    
 
     ?>
     @if($messagesIsShow)

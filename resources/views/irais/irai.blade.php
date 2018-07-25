7166ba008b31af2aa8a361f17ae74ec04f8d70b5
masahiko.morita <masahiko.a.morita@rakuten.com>
2018-07-25 04:11:55 +0000

    ribbon solved

--- resources/views/irais/irai.blade.php
+++ resources/views/irais/irai.blade.php
@@ -5,19 +5,23 @@
             <p></p>
             <a href="{{ route('irais.show', $irai->id) }}" > 
             <div class="maste-box">
+                @if($irai->alive == 0)
+                <div class=irai-finish-ribbon>
+                    <div>解決済み</div>
+                </div>    
+                @endif
                 <span class='maste-tape'>From {{$user->name}}</span>
             @if ($irai->id)
                 @if (Auth::user()->is_finishing($irai->id))
                     <div class='maste-content'>
                             <br>
                             <h4 class='under'>{{ $irai->title }}</h4>
-                            
                             <div id='maste-ryaku'>
                                 <h4 class="glyphicon glyphicon-time text-center"></h4><br> {{ $irai->start }}~<br>{{ $irai->finish }}<br>
                                 <h4 class="glyphicon glyphicon-map-marker"></h4> {{$irai->station}}<br>
                                 <h4 class="glyphicon glyphicon-gift"></h4> {{ $irai->reward }}<br>
                             </div>
-                        </div>
+                    </div>
                 @else
                     <div class='maste-content'>
                             <br>

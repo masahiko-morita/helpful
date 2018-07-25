64f4f8b8037ceea15d5a1cb736b9cfcb707389c7
mao.okunuki <mao.okunuki@rakuten.com>
2018-07-25 04:07:03 +0000

    aaa

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,71 +4,64 @@
 @section('content')
 
 <div class='container　col-xs-6"'>
-<div class='containermypage'>
-	<div class="introduction_momo">
-            <img src="{{ Gravatar::src($user->email, 100) . '&d=mm' }}" alt="" class="img-circle" width='30%'>
-        <div id=momo_name>
-            <h3>{{ $user->name }}</h3>
-        </div>
-        <div class='col-xs-5 text-center'>
-		    <div class="balloon">
-			    <div id="hensyuu_contents">
-      		        {{$user->content}}
-       		    </div>
-      		     @if (Auth::user()->id == $user->id)
-      		    <div id="hensyuu2">
-                        {!! Form::open(['route' => ['users.edit', $user->id], 'method' => 'get']) !!}
-                        {!! Form::submit('編集', ['class' => 'btn center-block']) !!}
-                        {!! Form::close() !!}
+    <div class='containermypage'>
+        	<div class="col-xs-12" id="introduction_momo">
+                    <img src="{{ Gravatar::src($user->email, 100) . '&d=mm' }}" alt="" class="img-circle" width='30%'>
+                <div id=momo_name>
+                    <h3>{{ $user->name }}</h3>
                 </div>
-                @endif  
-	        </div>
-	    </div>
-    </div>
-    <div class='maepage'>
-    	{!! $irais->render() !!}
-    </div>   
-    <div role="document" data-spy="scroll" data-target="#sampleScrollSpy">
-	    <div class="container-fluid">
-			<div class="row">
-			</div>
+                <div class='col-xs-5 text-center'>
+        		    <div class="balloon">
+        			    <div id="hensyuu_contents">
+              		        {{$user->content}}
+               		    </div>
+              		     @if (Auth::user()->id == $user->id)
+              		    <div id="hensyuu2">
+                                {!! Form::open(['route' => ['users.edit', $user->id], 'method' => 'get']) !!}
+                                {!! Form::submit('編集', ['class' => 'btn center-block']) !!}
+                                {!! Form::close() !!}
+                        </div>
+                        @endif  
+        	        </div>
+        	    </div>
+            </div>
+    <!--<div class='maepage'>-->
+    <!--	{!! $irais->render() !!}-->
+    <!--</div>   -->
+   
 
-<div class="col-xs-12" id='tatesen'>
-	<div id="sampleMainContents">
-		<h2 class="text-center">
-			<div class='ribbon3'>
-				<h2>出した依頼</h2></h2>
-			</div>
+            <div class="col-xs-12" id='tatesen'>
+            	<div id="sampleMainContents">
+            		<h2 class="text-center">
+            			<div class='ribbon3'>
+            				<h2>出した依頼</h2></h2>
+            			</div>
             			@if($count_irais==0)
-						<br>
-						 <h4 class='text-center'>投稿がありません</h4><br>
-						@else
-						<ol>
-							@include('irais.irai', ['irais' => $irais])
-						</ol>
-						@endif
-				</div>
-			</div>
-		</div>
+            			<br>
+            			 <h4 class='text-center'>投稿がありません</h4><br>
+            			@else
+            			<ol>
+            				@include('irais.irai', ['irais' => $irais])
+            			</ol>
+            			@endif
+            	</div>
+            </div>
 	</div>
-</div>
-
- <?php 
-        $v =$_COOKIE["data1"]??"";
-    
-    ?>
+     <?php 
+            $v =$_COOKIE["data1"]??"";
+        
+        ?>
  	
  	
     <div class="wrap-1 ball">
-@if($v == "")
-                	<div class="alert alert-warning alert-dismissible fade in" role="alert" id='alert'>
-                	<button type="button" data-dismiss="alert" class="close" onclick="document.cookie = 'data1=123';">&times;</button>
-                	<p class="text-center"><strong>新規投稿は<br>↓こちらから↓</strong></p>
-                    </div>
-@endif
-        <a href="{{ route('irais.create') }}">
-                <button class="btn-post"><i class="fa fa-hand-peace" id='peace'></i><br>New Post</button>
+        @if($v == "")
+        	<div class="alert alert-warning alert-dismissible fade in" role="alert" id='alert'>
+        	<button type="button" data-dismiss="alert" class="close" onclick="document.cookie = 'data1=123';">&times;</button>
+        	<p class="text-center"><strong>新規投稿は<br>↓こちらから↓</strong></p>
             </div>
+        @endif
+        <a href="{{ route('irais.create') }}">
+                <button class="btn-post"><i class="fa fa-hand-peace" id='peace'></i><br>New Post</button> 
         </a>
     </div>
 
@@ -77,8 +70,8 @@
             <p>{{ $irais->links() }}</p>
         </ul>
     </div>
+    
 </div>
 
 
-</a>
 @endsection
